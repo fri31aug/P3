8d96b19782d4fa416dcbca572d5eaa7e98a5f2f5
Ananya <96532503+fri31aug@users.noreply.github.com>
2023-11-19 02:07:52 -0700

    Update proc_filesys.c

--- proc_filesys.c
+++ proc_filesys.c
@@ -16,9 +16,8 @@ static ssize_t write_to_kernel(struct file *file, const char __user *user_buffer
 
     // TODO 1: copy from the user buffer to kernel buffer by using copy_from_user()
     // Hint: Only one line of code is needed
-    // copy_from_user returns the number of bytes that could not be copied. On success, this will be zero.
-    if (copy_from_user(kernel_buffer, user_buffer, min(count, MAX_MESSAGE_SIZE)))
-        return -EFAULT; // Return an error if the copy was not successful
+    if (copy_from_user(kernel_buffer, user_buffer, min((size_t)count, (size_t)MAX_MESSAGE_SIZE)))
+        return -EFAULT;
 
     pr_info("Received from user space: %s\n", kernel_buffer);
     return count;
@@ -26,23 +25,19 @@ static ssize_t write_to_kernel(struct file *file, const char __user *user_buffer
 
 static ssize_t read_from_kernel(struct file *file, char __user *user_buffer, size_t count, loff_t *ppos)
 {
-    ssize_t retval;
     size_t len = strlen(kernel_buffer);
-
-    if (*ppos > len)
-        return 0; // End of buffer reached
+    size_t to_copy = min(count, len - *ppos);
 
     // TODO 2: 
     // General solution: initialize retval to count
     // Bonus solution for Test Case 5: initialize retval to the minimum value between count and the length of the message
     // Hint: Only one line of code is needed
-    retval = min(count, len - *ppos);
+    ssize_t retval = to_copy;
 
     // TODO 3: copy from the kernel buffer to user buffer by using copy_to_user()
     // Hint: Only one line of code is needed
-    // copy_to_user returns the number of bytes that could not be copied. On success, this will be zero.
     if (copy_to_user(user_buffer, kernel_buffer + *ppos, retval))
-        return -EFAULT; // Return an error if the copy was not successful
+        return -EFAULT;
 
     // TODO 4: update the current position of the file
     // Hint: Only one line of code is needed
@@ -55,8 +50,6 @@ static ssize_t read_from_kernel(struct file *file, char __user *user_buffer, siz
 }
 
 static const struct proc_ops proc_operations = {
-    // TODO 5: assign the correct function pointers to the correct functions
-    // Hint: Only two lines of code is needed
     .proc_write = write_to_kernel,
     .proc_read = read_from_kernel,
 };
@@ -64,7 +57,6 @@ static const struct proc_ops proc_operations = {
 static int __init vfs_init(void)
 {
     // TODO 6: create a proc entry at /proc/my_proc_file
-    // Hint: Only one line of code is needed
     proc_create("my_proc_file", 0666, NULL, &proc_operations);
 
     pr_info("Module loaded\n");
@@ -74,7 +66,6 @@ static int __init vfs_init(void)
 static void __exit vfs_exit(void)
 {
     // TODO 7: remove the proc entry at /proc/my_proc_file
-    // Hint: Only one line of code is needed
     remove_proc_entry("my_proc_file", NULL);
 
     pr_info("Module unloaded\n");
