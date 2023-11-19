9ca611a8f8f9ee845d90ee59a109a69c78178eca
Ananya <96532503+fri31aug@users.noreply.github.com>
2023-11-19 02:26:18 -0700

    Update proc_filesys.c

--- proc_filesys.c
+++ proc_filesys.c
@@ -26,30 +26,31 @@ static ssize_t write_to_kernel(struct file *file, const char __user *user_buffer
 static ssize_t read_from_kernel(struct file *file, char __user *user_buffer, size_t count, loff_t *ppos)
 {
     size_t len = strlen(kernel_buffer);
-    size_t to_copy = min(count, len - *ppos);
+    size_t to_copy;
 
     // TODO 2: 
     // General solution: initialize retval to count
     // Bonus solution for Test Case 5: initialize retval to the minimum value between count and the length of the message
     // Hint: Only one line of code is needed
-    ssize_t retval = to_copy;
+    to_copy = min(count, len - (size_t)(*ppos));
 
     // TODO 3: copy from the kernel buffer to user buffer by using copy_to_user()
     // Hint: Only one line of code is needed
-    if (copy_to_user(user_buffer, kernel_buffer + *ppos, retval))
+    if (copy_to_user(user_buffer, kernel_buffer + *ppos, to_copy))
         return -EFAULT;
 
     // TODO 4: update the current position of the file
     // Hint: Only one line of code is needed
-    *ppos += retval;
+    *ppos += to_copy;
 
-    pr_info("Read %zd bytes from kernel space starting at position %lld\n", retval, *ppos);
-    pr_info("Read from kernel space: %.*s\n", (int)retval, kernel_buffer + *ppos);
+    pr_info("Read %zd bytes from kernel space starting at position %lld\n", to_copy, *ppos);
+    pr_info("Read from kernel space: %.*s\n", (int)to_copy, kernel_buffer + *ppos);
 
-    return retval;
+    return to_copy;
 }
 
 static const struct proc_ops proc_operations = {
+    // TODO 5: assign the correct function pointers to the correct functions
     .proc_write = write_to_kernel,
     .proc_read = read_from_kernel,
 };
