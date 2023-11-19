3b6bc9c9500ce9e105a1c6dddc428810bb96ecd9
Ananya <96532503+fri31aug@users.noreply.github.com>
2023-11-19 01:45:22 -0700

    Update proc_filesys.c

--- proc_filesys.c
+++ proc_filesys.c
@@ -11,55 +11,45 @@ static ssize_t write_to_kernel(struct file *file, const char __user *user_buffer
 {
     // TODO Bonus 1: check if the user is trying to write more than MAX_MESSAGE_SIZE bytes
     // if so, return -EINVAL (Invalid argument)
-
     if (count > MAX_MESSAGE_SIZE)
-    return -EINVAL;
-
-
+        return -EINVAL;
 
     // TODO 1: copy from the user buffer to kernel buffer by using copy_from_user()
     // Hint: Only one line of code is needed
+    // copy_from_user returns the number of bytes that could not be copied. On success, this will be zero.
+    if (copy_from_user(kernel_buffer, user_buffer, min(count, MAX_MESSAGE_SIZE)))
+        return -EFAULT; // Return an error if the copy was not successful
 
-    copy_from_user(kernel_buffer, user_buffer, count);
-
-
-
-    pr_info("Received from user space: %s", kernel_buffer);
+    pr_info("Received from user space: %s\n", kernel_buffer);
     return count;
 }
 
 static ssize_t read_from_kernel(struct file *file, char __user *user_buffer, size_t count, loff_t *ppos)
 {
     ssize_t retval;
+    size_t len = strlen(kernel_buffer);
 
-    if (*ppos > strlen(kernel_buffer))
-        return -EINVAL;
+    if (*ppos > len)
+        return 0; // End of buffer reached
 
     // TODO 2: 
     // General solution: initialize retval to count
     // Bonus solution for Test Case 5: initialize retval to the minimum value between count and the length of the message
     // Hint: Only one line of code is needed
-
-    retval = min(count, strlen(kernel_buffer));
-
-
+    retval = min(count, len - *ppos);
 
     // TODO 3: copy from the kernel buffer to user buffer by using copy_to_user()
     // Hint: Only one line of code is needed
-
-    copy_to_user(user_buffer, kernel_buffer, retval);
-
-
-
-    pr_info("Read %zd bytes from kernel space starting at position %lld\n", retval, *ppos);
-    pr_info("Read from kernel space: %.*s\n", (int)retval, kernel_buffer + *ppos);
+    // copy_to_user returns the number of bytes that could not be copied. On success, this will be zero.
+    if (copy_to_user(user_buffer, kernel_buffer + *ppos, retval))
+        return -EFAULT; // Return an error if the copy was not successful
 
     // TODO 4: update the current position of the file
     // Hint: Only one line of code is needed
-
     *ppos += retval;
 
-
+    pr_info("Read %zd bytes from kernel space starting at position %lld\n", retval, *ppos);
+    pr_info("Read from kernel space: %.*s\n", (int)retval, kernel_buffer + *ppos);
 
     return retval;
 }
@@ -67,22 +57,15 @@ static ssize_t read_from_kernel(struct file *file, char __user *user_buffer, siz
 static const struct proc_ops proc_operations = {
     // TODO 5: assign the correct function pointers to the correct functions
     // Hint: Only two lines of code is needed
-
     .proc_write = write_to_kernel,
     .proc_read = read_from_kernel,
-
-
-
 };
 
 static int __init vfs_init(void)
 {
     // TODO 6: create a proc entry at /proc/my_proc_file
     // Hint: Only one line of code is needed
-
     proc_create("my_proc_file", 0666, NULL, &proc_operations);
-
-
 
     pr_info("Module loaded\n");
     return 0;
@@ -92,10 +75,7 @@ static void __exit vfs_exit(void)
 {
     // TODO 7: remove the proc entry at /proc/my_proc_file
     // Hint: Only one line of code is needed
-
     remove_proc_entry("my_proc_file", NULL);
-
-
 
     pr_info("Module unloaded\n");
 }
@@ -103,7 +83,7 @@ static void __exit vfs_exit(void)
 module_init(vfs_init);
 module_exit(vfs_exit);
 
-MODULE_VERSION("0.1");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Ananya Yadav");
-MODULE_DESCRIPTION("CSE330 Project 3 Virtual File System\n");
+MODULE_DESCRIPTION("CSE330 Project 3 Virtual File System");
+MODULE_VERSION("0.1");
